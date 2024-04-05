3565db67a23ef857bed8ea5a6633ed4f86b069f2
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-05 19:29:29 +0200

    arreglo de img

--- src/Utils/DataSocials.js
+++ src/Utils/DataSocials.js
@@ -2,14 +2,14 @@
 export const DataSocials = [
   {
     id: 1,
-    img: "./src/assets/github_git_icon_145985.png",
+    img: "./src/assets/img/Socials/github_git_icon_145985.png",
     url: "https://github.com/Pibe0010",
     name: "github",
   },
 
   {
     id: 3,
-    img: "./src/assets/linkedin.png",
+    img: "./src/assets/img/Socials/linkedin.png",
     url: "https://www.linkedin.com/in/daniel-montero-castro/",
     name: "linkedin",
   },
--- src/components/Animations/ImgPerfil.jsx
+++ src/components/Animations/ImgPerfil.jsx
@@ -1,13 +1,12 @@
 import "./ImgPerfil.css";
+import perfil from "../src/assets/img/mi-foto-perfil-Photoroom.png";
 
 export const ImgPerfil = () => {
   return (
     <div className="loader_one">
       <div className="loader_cube loader_cube--color"></div>
       <div className="loader_cube loader_cube--glowing">
-        <img
-          className="img-about"
-          src="./src/assets/mi-foto-perfil-Photoroom.png"></img>
+        <img className="img-about" src={perfil}></img>
       </div>
     </div>
   );
