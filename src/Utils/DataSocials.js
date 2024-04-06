6e83665aa6837d857f1a9d1bfb79184e52288f92
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-06 12:12:16 +0200

    arreglo de imagen de redes

--- src/Utils/DataSocials.js
+++ src/Utils/DataSocials.js
@@ -1,18 +1,15 @@
-import github from "../assets/img/Socials/github_git_icon_145985.png";
-import linkedin from "../assets/img/Socials/linkedin.png";
-
 //  redes sociales
 export const DataSocials = [
   {
     id: 1,
-    img: { github },
+    img: "../assets/img/Socials/github_git_icon_145985.png",
     url: "https://github.com/Pibe0010",
     name: "github",
   },
 
   {
     id: 3,
-    img: { linkedin },
+    img: "../assets/img/Socials/linkedin.png",
     url: "https://www.linkedin.com/in/daniel-montero-castro/",
     name: "linkedin",
   },
