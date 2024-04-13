f14ff32e622a21f6686d9f3c4f4bd62f500c980b
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-13 13:58:17 +0200

    eliminacion de iconos

--- src/components/Icons.jsx
+++
@@ -1,8 +0,0 @@
-import "./Icons.css";
-export function Icons({ name, className, id }) {
-  return (
-    <span className={`material-symbols-outlined ${className}`} id={id}>
-      {name}
-    </span>
-  );
-}
